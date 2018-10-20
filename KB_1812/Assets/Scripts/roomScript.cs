f4932f7039f1b4b2035afedd8939b778cbd77f4b
miyass <exe.miyakura0629@gmail.com>
2018-10-21 04:23:58 +0900

    connect firebase in RoomScene

--- KB_1812/Assets/Scripts/roomScript.cs
+++ KB_1812/Assets/Scripts/roomScript.cs
@@ -3,24 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 
 public class roomScript : MonoBehaviour {
     InputField roomInput;
     string roomNumber;
     Text outText;
 
+    //Firebaseの設定用変数
+    Firebase.Auth.FirebaseAuth auth;
+    Firebase.Auth.FirebaseUser loginUser;
+    DatabaseReference reference;
+
 	// Use this for initialization
     void OnEnable () {
         roomInput = GameObject.Find("InputField").GetComponent<InputField>();
         outText = GameObject.Find("outText").GetComponent<Text>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void Start()
+    {
+        //auth関係初期設定
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        loginUser = auth.CurrentUser;
+        //database関係初期設定
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://kb-1812.firebaseio.com/");
+        reference = FirebaseDatabase.DefaultInstance.RootReference;    
+
+        if (loginUser != null)
+        {
+            Debug.Log("ログイン出来てるんご");
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
     public void madeRoomButton(){
+        //ここではFirebase関係の処理はなし
+        //次のシーンで、与えられた緯度、経度
+        //Parkerからここのシーンはいただいてちょ
         SceneManager.LoadScene("");
     }
 
@@ -34,6 +61,17 @@ public class roomScript : MonoBehaviour {
             outText.text = "ルームナンバーが違います";
             roomInput.text = "";
         }
+        //firebase関係の処理メソッド
+        joinRoom(room_text);
 
+    }
+
+    void joinRoom(string room_id) {
+        if (loginUser == null) {
+            Debug.Log("ログイン出来てないよin JoinRoom");
+        }
+        User user = new User(loginUser.DisplayName, false, 0);
+        string userJson = JsonUtility.ToJson(user);
+        reference.Child("rooms").Child(room_id).Child("users").Child(loginUser.UserId).SetRawJsonValueAsync(userJson);  
     }
 }
