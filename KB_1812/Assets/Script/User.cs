8c374fba6d42369cb6de4eae266b8ac3b5d58880
Ryota Tomiyama <lit.karu.noir@gmail.com>
2018-10-21 03:20:54 +0900

    クラス定義

---
+++ KB_1812/Assets/Script/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User {
+
+    public string name;
+    public bool isGoal;
+    public int score;
+
+    public User(string name, bool isGoal, int score)
+    {
+        this.name = name;
+        this.isGoal = isGoal;
+        this.score = score;
+    }
+}
