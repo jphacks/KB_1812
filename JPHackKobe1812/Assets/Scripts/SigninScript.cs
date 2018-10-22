42da4abbaa18431057f44e0584a133a41bcad5e8
Ryota Tomiyama <lit.karu.noir@gmail.com>
2018-10-22 16:09:07 +0900

    jsonを除外

--- JPHackKobe1812/Assets/Scripts/SigninScript.cs
+++ JPHackKobe1812/Assets/Scripts/SigninScript.cs
@@ -51,7 +51,7 @@ public class SigninScript : MonoBehaviour {
 
         // TODO 飛ぶシーンが違う
         //if (isMove) SceneManager.LoadScene("Main");
-        if (isMove) Debug.Log("シーン移動と切り替える！");
+        if (isMove) SceneManager.LoadScene("SelectRoom");
     }
 
     public void OnClick(){
